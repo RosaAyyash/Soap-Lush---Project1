3247a5dedbea8c7153ca4b955860024d113bbd14
RosaAyyash02 <rozie.ayyash2002@gmail.com>
2023-01-07 06:20:26 +0200

    First Comit

---
+++ Backend/SoapLush/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿namespace SoapLush.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        public string Ingredients { get; set; } = string.Empty;
+
+        public string Image { get; set; } = string.Empty;
+
+        public double Price { get; set; }
+
+        public int CategoryId { get; set; } //foreign key
+        public Category? Category { get; set; } //Reference Navigation
+
+        public int SubCategoryId { get; set; } //foreign key
+
+        public SubCategory? SubCategory { get; set; } //Reference Navigation
+    }
+}
